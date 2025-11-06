24d5fbc49eccd79610f4d587f66531965296259b
bdmorales5 <bdmorales5@cps.edu>
2025-11-06 19:19:27 +0000

    string properties and Advanced Operations

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -71,12 +71,21 @@ print(joined_motto_split)
 
 
 # Problem Set 4: String Properties and Advanced Operations
-# Repetition:
-# Concatenate the word "Iteration" 7 times.
+# 1. Repetition
+print("Iteration" * 7)
 
-# Word Search:
-# Check if the word "moonlight" appears in the quote: "With freedom, books, flowers, and the moon, who could not be happy? - Oscar Wilde"
+# 2. Word Search
+quote = "With freedom, books, flowers, and the moon, who could not be happy? - Oscar Wilde"
+if "moonlight" in quote:
+    print("The word 'moonlight' appears in the quote.")
+else:
+    print("The word 'moonlight' does not appear in the quote.")
 
-# Length and Count:
-# a. Calculate the number of characters (including spaces and punctuation) in the word/phrase: "Supercalifragilisticexpialidocious".
-# b. Count the number of times the letter 'i' appears in the same word/phrase.
+# 3. Length and Count
+word = "Supercalifragilisticexpialidocious"
+
+# a. Number of characters
+print(len(word))
+
+# b. Number of 'i' letters
+print(word.count('i'))
